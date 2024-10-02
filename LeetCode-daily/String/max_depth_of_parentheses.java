1fd433793fd3300abf6662df449e6cce562955a4
Aryan Nishen <127716401+nishenaryan14@users.noreply.github.com>
2024-10-02 19:53:57 +0530

    Create max_depth_of_parentheses.java

---
+++ LeetCode-daily/String/max_depth_of_parentheses.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxDepth(String s) {
+        int maxDepth = 0;
+        int depth = 0;
+
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                depth++;
+                maxDepth = Math.max(maxDepth,depth);
+            }
+            else if(s.charAt(i)==')'){
+                depth--;
+            }
+        }
+        return maxDepth;
+    }
+}
